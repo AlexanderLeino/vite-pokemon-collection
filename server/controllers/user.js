8a35dba5896878b86c629078d6357c454fa49d6d
Alexander Leino <Alexander.Leino@yahoo.com>
2023-02-04 14:14:41 -0500

    error codes are working on test query

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,75 +1,87 @@
 const axios = require("axios");
 const bycrpt = require("bcrypt");
-const {signToken} = require('../utlis') 
+const { signToken } = require("../utlis");
 require("dotenv").config();
 const User = require("../models/User.js");
 
 module.exports = {
   signIn: async ({ body }, res) => {
-    let token
+    let token;
     try {
-      let { userName, password } = body
+      let { userName, password } = body;
       let response = await User.findOne({ userName });
-      
+
       const match = await bycrpt.compare(password, response.password);
-      if(match) {
-        token = signToken(response)
+      if (match) {
+        token = signToken(response);
       }
-      res.send({token}).status(200)
+      res.send({ token }).status(200);
     } catch (e) {
-      res.send({message: e.message}).status(400);
+      res.send({ message: e.message }).status(400);
     }
   },
   createUser: async ({ body }, res) => {
     try {
-        let {userName, password, email} = body
-        let response = await User.create({ userName, password, email });
-        let token = signToken(response)
-        res.send(token).status(200);
+      let { userName, password, email } = body;
+      let response = await User.create({ userName, password, email });
+      let token = signToken(response);
+      res.send(token).status(200);
     } catch (e) {
-      res.send({message: e.message}).status(400);
+      res.send({ message: e.message }).status(400);
     }
-
   },
-  updateCardList: async ({body}, res) => {
+  updateCardList: async ({ body }, res) => {
     try {
-        let {cardData, userId} = body.data
-        
-      
-        let foundUser = await User.findOne({_id: userId}).elemMatch('cards', {name: cardData.name, cardNumber: cardData.cardNumber})
-        console.log('Found User', foundUser)
-        if(foundUser){ 
-          // await User.updateOne({})
-        } else {
-          await User.updateOne({_id: userId}, {
+      let { cardData, userId } = body.data;
+
+      let foundUser = await User.findOne({ _id: userId }).elemMatch("cards", {
+        name: cardData.name,
+        cardNumber: cardData.cardNumber,
+      });
+      console.log("Found User", foundUser);
+      if (foundUser) {
+        // await User.updateOne({})
+      } else {
+        await User.updateOne(
+          { _id: userId },
+          {
             $push: {
-                cards: cardData
-            }
-        })
-        }
+              cards: cardData,
+            },
+          }
+        );
+      }
 
-
-      
-      res.send({message: "User was successfully updated! :)"}).status(200)
+      res.send({ message: "User was successfully updated! :)" }).status(200);
     } catch (e) {
-      res.send({message: e.message}).status(500)
+      res.send({ message: e.message }).status(500);
     }
   },
-  findCardSubDoc: async ({body} , res) => {
+  findCardSubDoc: async ({ body }, res) => {
     
-    let foundDocument = User.findOne({_id: body.data}).elemMatch('cards', {name: "Umbreon", cardNumber: "215"}).select("cards.$").exec(async function(err, doc){
-      doc.cards[0].quantity = doc.cards[0].quantity + 1
-      let updatedUser = await User.findOneAndUpdate({_id: body.data}, {
-          cards: doc.cards[0]
-      
-      })
-
-      console.log("Updated User", updatedUser)
-    })
-
-
-    
-    
-  }
-  
+    try {
+      User.findOne({ _id: body.data })
+        .elemMatch("cards", { name: "Umbsdfreon", cardNumber: "215" })
+        .select("cards.$")
+        .exec(async function (err, doc) {
+              if(doc) {
+                doc.cards[0].quantity = doc?.cards[0]?.quantity + 1;
+                await User.findOneAndUpdate(
+                  { _id: body.data },
+                  {
+                    cards: doc.cards[0],
+                  }
+                );
+                res.status(200).send("Congrats your user has been updated!")
+              }
+                else {
+                  throw new Error("Couldnt Find Specified Card on User :/")
+                }
+        });
+        
+        
+    } catch (e) {
+      res.status(500).send({ message: e.message });
+    }
+  },
 };
