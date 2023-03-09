5d99e9d0520410a1a14e0c15abc0bdfc356b8b92
Alexander Leino <Alexander.Leino@yahoo.com>
2023-03-09 16:23:13 -0500

    sign up form is responsive

--- src/components/Form/AccountCreation/createAccount.tsx
+++ src/components/Form/AccountCreation/createAccount.tsx
@@ -4,10 +4,10 @@ import Input from '../../Input'
 import Flex from '../../Flex'
 import Label from '../../label'
 import AuthService from '../../../utlis/Auth'
-import stockImg from '../../../assets/stockImg.svg'
+import useWindowDimensions from '../../../hooks/getWindowDimensions'
 const localStorage = window.localStorage
 export const CreateAccount = () => {
-
+  const {width , height } = useWindowDimensions()
   const [accountInfo, setAccountInfo] = useState({ userName: "", password: '', email: '' })
 
   const handleSubmit = async (e: any) => {
@@ -22,18 +22,18 @@ export const CreateAccount = () => {
     setAccountInfo({ ...accountInfo, [e.target.name]: e.target.value })
   };
   return (
-    <div className='flex flex-col justify-start w-full bg-orange-300 items-center rounded-tl-3xl rounded-bl-3xl h-full' >
+    <div className={`flex flex-col justify-start w-full bg-orange-300 items-center ${width > 1200 ? "rounded-tr-3xl rounded-br-3xl" : "rounded-bl-3xl rounded-br-3xl"}  h-full`} >
       <form onSubmit={(e) => handleSubmit(e)} className='h-fit flex flex-col items-center justify-center w-full '>
         <Flex flexDirection='flex-col' alignItems='items-center' justifyContent='justify-center' height='h-full'>
-          <div className='font-bold text-6xl mt-40 text-orange-500'>Get started</div>
+          <div className={`font-bold text-6xl ${width > 1200 ? "mt-40" : "mt-5"} text-orange-500`}>Get started</div>
           <div className='mt-2 text-orange-400 font-bold text-2xl'>Create your account now</div>
         </Flex>
         <Flex justifyContent='justify-center' alignItems='items-center' flexDirection='flex-col' borderRadius='rounded-md' marginTop='mt-5' height='h-full'>
-          <Input label='Email' type='email' name={'email'} margin={'mt-2'}  required onChange={handleChange} />
-          <Input label='Username' type='string' name={'userName'} margin={'mt-5'}  width={'w-full'} required onChange={handleChange} />
-          <Input label='Password' name={'password'} width={'w-full'} margin={'mt-5'}  type='password' required onChange={handleChange} />
-          <Button onClick={handleSubmit} margin='mt-5' width='w-full' border='border-none'>Submit</Button>
-          <div className='mt-2 text-orange-500'>Have an account?<a className='ml-2 font-bold text-orange-500' href='/login'>Login</a></div>
+          <Input label='Email' type='email' name={'email'} margin={'mt-0'}  required onChange={handleChange} />
+          <Input label='Username' type='string' name={'userName'} margin={'mt-4'}  width={'w-full'} required onChange={handleChange} />
+          <Input label='Password' name={'password'} width={'w-full'} margin={'mt-4'}  type='password' required onChange={handleChange} />
+          <Button onClick={handleSubmit} margin='mt-4' width='w-full' border='border-none'>Submit</Button>
+          <div className='mt-2 mb-5 text-orange-500'>Have an account?<a className='ml-2 font-bold text-orange-500' href='/login'>Login</a></div>
         </Flex>
       </form>
     </div>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,19 +2,39 @@ import React from 'react'
 import { CreateAccount } from '../components/Form/AccountCreation/createAccount'
 import Flex from '../components/Flex'
 import StockImg from '../assets/stock-women.svg'
+import useWindowDimensions from '../hooks/getWindowDimensions'
 export const Signup = () => {
+  
+  const { width } = useWindowDimensions()
+
   return (
-  <>
-  <div className='grid grid-cols-2 h-full'>
-        
-            <div className=' flex flex-col h-full items-center justify-start pt-[120px]'>
-              
-              <img style={{width: '500px', height: 'fit-content',}} src={StockImg}/>
-                <div className='text-4xl font-bold text-orange-200 mt-10 ml-2'>Start Your Collection <span className='font-extrabold underline'>Today!</span></div>
-      
+    <>
+      {
+        width < 1200
+          ?
+          <div className='flex flex-col-reverse'>
+
+            <div className=' flex flex-col h-full items-center justify-start '>
+
+              <img className='mt-10' style={{ width: '500px', height: 'fit-content', }} src={StockImg} />
+              <div className='text-4xl font-bold text-orange-200 mt-10 ml-2'>Start Your Collection <span className='font-extrabold underline'>Today!</span></div>
+
             </div>
-              <CreateAccount />
-  </div>
-  </>
+            <CreateAccount />
+          </div>
+          :
+          <div className='grid grid-cols-2 h-full'>
+
+            <CreateAccount />
+            <div className=' flex flex-col h-full items-center justify-start pt-[120px]'>
+
+              <img style={{ width: '500px', height: 'fit-content', }} src={StockImg} />
+              <div className='text-4xl font-bold text-orange-200 mt-10 ml-2'>Start Your Collection <span className='font-extrabold underline'>Today!</span></div>
+
+            </div>
+          </div>
+      }
+
+    </>
   )
 }
